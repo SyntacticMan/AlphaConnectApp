895091bcd9e4decd38fc6263116514d57e78cb05
SyntacticMan <nuno.meren@gmail.com>
2015-07-07 11:41:07 +0100

    Initial commit

---
+++ AlphaConnectApp/PlatoonModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlphaConnectApp
+{
+    class PlatoonModel
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public DateTime initialDate { get; set; }
+        public DateTime finalDate { get; set; }
+    }
+}
