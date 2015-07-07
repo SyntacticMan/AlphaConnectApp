8fb93d7224ff6a692214970f6ec7c8a9f1ba5eb1
SyntacticMan <nuno.meren@gmail.com>
2015-07-07 11:52:51 +0100

    Added WebConnectionTool

---
+++ AlphaConnectApp/WebConnectionTool.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlphaConnectApp
+{
+    class WebConnectionTool
+    {
+        public async Task<string> GetXmlFromService(string url, string endpoint, string value)
+        {
+            Task<string> response;
+
+            try
+            {
+                string address = "";
+
+                if (string.IsNullOrEmpty(value))
+                {
+                    address = string.Format("{0}/{1}", url, endpoint);
+                }
+                else
+                {
+                    address = string.Format("{0}/{1}/{2}", url, endpoint, value);
+                }
+
+                HttpClient client = new HttpClient();
+
+                response = client.GetStringAsync(address);
+
+                return response.Result;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return "";
+        }
+    }
+}
